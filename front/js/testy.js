c77cce846dee207cd55cf4dc28212e196e4dc611
Mmarcopollo <szafranmarek97@gmail.com>
2019-11-17 21:58:33 +0100

    Get test method in front

--- front/js/testy.js
+++ front/js/testy.js
@@ -1,9 +1,12 @@
 var RowdyruffBoys = window.RowdyruffBoys || {};
 RowdyruffBoys.map = RowdyruffBoys.map || {};
 
+let json;
 
 (function checkIfSignedIn($) {
-	var authToken;
+    var authToken;
+    
+    
     RowdyruffBoys.authToken.then(function setAuthToken(token) {
         if (token) {
             authToken = token;
@@ -14,11 +17,53 @@ RowdyruffBoys.map = RowdyruffBoys.map || {};
         alert(error);
         window.location.href = '/signin.html';
     });
+
+    function getTest() {
+        $.ajax({
+            method: 'POST',
+            url: _config.api.invokeUrl + '/ride',
+            headers: {
+                Authorization: authToken
+            },
+            data: JSON.stringify({
+                "test_id": 3
+            }),
+            contentType: 'application/json',
+            success: (data) => {
+                //console.log(data);
+                json = data;
+            },
+        });
+    }
+
+
+    
 }(jQuery));
 
 
-jQuery(document).ready(function($) {
-    $(".clickable-row").click(function() {
+jQuery(document).ready(function ($) {
+    $(".clickable-row").click(function () {
         window.location = $(this).data("href");
     });
- });
+});
+
+
+Survey
+        .StylesManager
+        .applyTheme("default");
+
+    window.survey = new Survey.Model(json);
+
+    survey
+        .onComplete
+        .add(function (result) {
+            document
+                .querySelector('#survey')
+                .textContent = "Result JSON:\n" + JSON.stringify(result.data, null, 3);
+        });
+
+    $("#surveyContainer").Survey({ model: survey });
+
+
+
+
