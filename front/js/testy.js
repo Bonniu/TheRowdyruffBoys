752dff524986c04d662142d139f7daaea56d79ab
Mmarcopollo <szafranmarek97@gmail.com>
2019-11-18 10:48:39 +0100

    Almost done

--- front/js/testy.js
+++ front/js/testy.js
@@ -1,12 +1,14 @@
+
+
 var RowdyruffBoys = window.RowdyruffBoys || {};
 RowdyruffBoys.map = RowdyruffBoys.map || {};
 
-let json;
+let json = {
+
+};
 
 (function checkIfSignedIn($) {
     var authToken;
-    
-    
     RowdyruffBoys.authToken.then(function setAuthToken(token) {
         if (token) {
             authToken = token;
@@ -18,27 +20,33 @@ let json;
         window.location.href = '/signin.html';
     });
 
-    function getTest() {
-        $.ajax({
-            method: 'POST',
-            url: _config.api.invokeUrl + '/ride',
-            headers: {
-                Authorization: authToken
-            },
-            data: JSON.stringify({
-                "test_id": 3
-            }),
-            contentType: 'application/json',
-            success: (data) => {
-                //console.log(data);
-                json = data;
-            },
-        });
-    }
 
 
-    
+
 }(jQuery));
+
+let url = "https://0hqj2kdr41.execute-api.us-east-1.amazonaws.com/testTable/testtable";
+
+let data = {
+    "test_id": 3
+}
+
+let headers = {
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': RowdyruffBoys.authToken
+    }
+}
+
+axios.get(url, { params: { "test_id": 3 }}, headers)
+.then(function (response) {
+    json = response
+})
+.catch(function (error) {
+    console.log("dupa");
+})
+
+
 
 
 jQuery(document).ready(function ($) {
@@ -49,20 +57,20 @@ jQuery(document).ready(function ($) {
 
 
 Survey
-        .StylesManager
-        .applyTheme("default");
+    .StylesManager
+    .applyTheme("default");
 
-    window.survey = new Survey.Model(json);
+window.survey = new Survey.Model(json.data);
 
-    survey
-        .onComplete
-        .add(function (result) {
-            document
-                .querySelector('#survey')
-                .textContent = "Result JSON:\n" + JSON.stringify(result.data, null, 3);
-        });
+survey
+    .onComplete
+    .add(function (result) {
+        document
+            .querySelector('#survey')
+            .textContent = "Result JSON:\n" + JSON.stringify(result.data, null, 3);
+    });
 
-    $("#surveyContainer").Survey({ model: survey });
+$("#surveyContainer").Survey({ model: survey });
 
 
 
