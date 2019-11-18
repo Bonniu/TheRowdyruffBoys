16fe0c9ea6347d305e0fcf2c6b07e04181ab46ae
Mmarcopollo <szafranmarek97@gmail.com>
2019-11-18 13:38:26 +0100

    GetTest complete

--- front/js/testy.js
+++ front/js/testy.js
@@ -3,9 +3,7 @@
 var RowdyruffBoys = window.RowdyruffBoys || {};
 RowdyruffBoys.map = RowdyruffBoys.map || {};
 
-let json = {
-
-};
+let json;
 
 (function checkIfSignedIn($) {
     var authToken;
@@ -27,25 +25,38 @@ let json = {
 
 let url = "https://0hqj2kdr41.execute-api.us-east-1.amazonaws.com/testTable/testtable";
 
-let data = {
-    "test_id": 3
-}
 
-let headers = {
-    headers: {
+// let headers = {
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': RowdyruffBoys.authToken
+//     }
+// }
+
+$.ajax({
+    type:"GET",
+    url: url,
+    data: {      
         'Content-Type': 'application/json',
-        'Authorization': RowdyruffBoys.authToken
-    }
-}
+        'Authorization': RowdyruffBoys.authToken,
+        'test_id': 3
+      },
+    crossDomain: true,
+    success: function (data) {
+      console.log("received: " + JSON.stringify(data));
+      var survey = new Survey.Model(JSON.parse(data));
 
-axios.get(url, { params: { "test_id": 3 }}, headers)
-.then(function (response) {
-    json = response
-})
-.catch(function (error) {
-    console.log("dupa");
-})
+      survey
+          .onComplete
+          .add(function (result) {
+              document
+                  .querySelector('#survey')
+                  .innerHTML = "result: " + JSON.stringify(result.data);
+          });
 
+      $("#surveyContainer").Survey({model: survey});
+
+    }});
 
 
 
@@ -54,24 +65,5 @@ jQuery(document).ready(function ($) {
         window.location = $(this).data("href");
     });
 });
-
-
-Survey
-    .StylesManager
-    .applyTheme("default");
-
-window.survey = new Survey.Model(json.data);
-
-survey
-    .onComplete
-    .add(function (result) {
-        document
-            .querySelector('#survey')
-            .textContent = "Result JSON:\n" + JSON.stringify(result.data, null, 3);
-    });
-
-$("#surveyContainer").Survey({ model: survey });
-
-
 
 
