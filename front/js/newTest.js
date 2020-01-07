19bf45468d80060e882a04203abf937303032fe8
Kar0lLasek <19karolasek98@gmail.com>
2020-01-07 17:51:03 +0100

    Translator works for single words in titles and choices

--- front/js/newTest.js
+++ front/js/newTest.js
@@ -40,7 +40,7 @@ creator.saveSurveyFunc = function () {
         success: function(resp) { 
             console.log('on success');
 			alert(resp);
-			window.location.href = 'testy.html';
+			//window.location.href = 'testy.html';
         },
         error: function(resp, err) { 
             console.log('fail'); 
@@ -54,25 +54,27 @@ creator.saveSurveyFunc = function () {
 		//sprawdzamy czy "type":"text" czy "type":"radiogroup"
 		//jezeli "text" to tlumaczymy tylko "title"
 		//jezeli "radiogroup" to tlumaczymy "title" oraz tablice "choices"
+		
 		txt = JSON.parse(creator.text);
-		if(text != null) {
+		if(txt != null) {
 			for( let i = 0; i < txt.pages.length; i++) {
 				for( let j = 0; j < txt.pages[i].elements.length; j++) {
 					if(txt.pages[i].elements[j].type == "text") {
-						txt.pages[i].elements[j].title = getTranslatorEN_PL(txt.pages[i].elements[j].title);
+						getTranslatorEN_PL_title(txt.pages[i].elements[j].title, i, j);
 					} else if(txt.pages[i].elements[j].type == "radiogroup") {
-						txt.pages[i].elements[j].title = getTranslatorEN_PL(txt.pages[i].elements[j].title);
+						getTranslatorEN_PL_title(txt.pages[i].elements[j].title, i, j);
 						for(let t = 0; t < txt.pages[i].elements[j].choices.length; t++) {
-							txt.pages[i].elements[j].choices[t] = getTranslatorEN_PL(txt.pages[i].elements[j].choices[t]);
+							getTranslatorEN_PL_choices(txt.pages[i].elements[j].choices[t].text, i, j, t);
 						}
 					}
 				}
 			}
 		}
 		//nasz txt wysyłamy do DynamoDB jako od razu przetłumaczoną wersję.
-		//JEDNAK
-		//jest problem że getTranslatorEN_PL nie działa od razu i zwraca undefined i dupa...
-		alert(JSON.stringify(JSON.parse(creator.text)));
+		//DZIAŁA tylko dla pojedynczych słów XD
+		//alert(JSON.stringify(JSON.parse(creator.text)));
+		
+		//muszę zrobić oddzielne listy na przechowywanie tych tłumaczeń i muszą być oznaczone i wkładane na odpowiednie miejsce.
 		
 };
 
@@ -95,27 +97,11 @@ creator.text = "{}";
 var abc = "";
 $('#translate').click(function() {
 	getTranslatorEN_PL("bad");
-	
-	//DO PRZEMYŚLENIA
-	// function someFunc() {
-
-	// callAjaxfunc(function() {
-		// console.log('Pass2');
-	// });
-
-	// }
-
-	// function callAjaxfunc(callback) {
-		//All ajax calls called here
-		// onAjaxSuccess: function() {
-			// callback();
-		// };
-		// console.log('Pass1');    
-	// }
 });
 
-$('#getA').click(function() {
-	console.log(abc);
+$('#gettxt').click(function() {
+	//TO JEST DO dynamoDB do przekazania
+	console.log(JSON.stringify(txt));
 });
 
 function getTranslatorEN_PL(text) {
@@ -134,10 +120,66 @@ function getTranslatorEN_PL(text) {
 				var json = result;
 				if(json.def[0] != null) {
 					var r = json.def[0].tr[0].text
-					abc = r;
+					//abc = r;
 					console.log("tłumacz RU-PL: " + text_ + " --> " + r);
 					//console.log(this.change);
 					//return r;
+				} else {
+					alert("no traslate pl");
+				}
+			  }});
+		} else {
+			alert("no traslate ru");
+		}
+	  }});
+}
+
+function getTranslatorEN_PL_title(text, i, j) {
+	let url = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20191230T110607Z.67ee80aaff673d64.91f4ec360d12511dd6be5aaa55ae8b24ef99214f&lang=en-ru&text=";
+	url = url + text;
+	$.ajax({url: url, success: function(result){
+		console.log(result);
+		var json = result;
+		if(json.def[0] != null) {
+			text_ = json.def[0].tr[0].text;
+			console.log("tłumacz EN-RU: " + text + " --> " + text_);
+			let url_ = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20191230T110607Z.67ee80aaff673d64.91f4ec360d12511dd6be5aaa55ae8b24ef99214f&lang=ru-pl&text=";
+			url_ = url_ + text_;
+			$.ajax({url: url_, success: function(result){
+				console.log(result);
+				var json = result;
+				if(json.def[0] != null) {
+					var r = json.def[0].tr[0].text
+					txt.pages[i].elements[j].title = r;
+					console.log("tłumacz RU-PL: " + text_ + " --> " + r);
+				} else {
+					alert("no traslate pl");
+				}
+			  }});
+		} else {
+			alert("no traslate ru");
+		}
+	  }});
+}
+
+function getTranslatorEN_PL_choices(text, i, j, t) {
+	let url = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20191230T110607Z.67ee80aaff673d64.91f4ec360d12511dd6be5aaa55ae8b24ef99214f&lang=en-ru&text=";
+	url = url + text;
+	$.ajax({url: url, success: function(result){
+		console.log(result);
+		var json = result;
+		if(json.def[0] != null) {
+			text_ = json.def[0].tr[0].text;
+			console.log("tłumacz EN-RU: " + text + " --> " + text_);
+			let url_ = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20191230T110607Z.67ee80aaff673d64.91f4ec360d12511dd6be5aaa55ae8b24ef99214f&lang=ru-pl&text=";
+			url_ = url_ + text_;
+			$.ajax({url: url_, success: function(result){
+				console.log(result);
+				var json = result;
+				if(json.def[0] != null) {
+					var r = json.def[0].tr[0].text
+					txt.pages[i].elements[j].choices[t].text = r;
+					console.log("tłumacz RU-PL: " + text_ + " --> " + r);
 				} else {
 					alert("no traslate pl");
 				}
