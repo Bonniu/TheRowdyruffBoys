0a348f3a589a228d369b7caa3fcbca5cc6fe00cb
Sylwester <sylwek2d@wp.pl>
2020-01-19 22:38:36 +0100

    edit export and import

--- front/js/testy.js
+++ front/js/testy.js
@@ -76,7 +76,7 @@ creator
         visible: true,
         title: "Export to CSV",
         action: function () {
-            console.log(JSON.stringify(JSON.parse(creator.text)));
+            // console.log(JSON.stringify(JSON.parse(creator.text)));
             exportCSVFile(false, JSON.stringify(JSON.parse(creator.text)), "exported");
         }
     });
@@ -157,40 +157,73 @@ async function redirectToTestySecond(){
 function JSONToCSVConvertor(JSONData) {
     //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
     var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
-    var CSV = '';
-
-    var line = '';
-    for (var i = 0; i < arrData.length; i++) {
-        if (arrData[i] == '"' && arrData[i+1] == ':' && arrData[i+2] == '"') {
-            line=':';
-            i=i+2;
+    
+    var endfile='';
+    var a='';
+    var number = 1;
+    console.log(arrData);
+    for(var i = 0; i < arrData.length; i++)
+    {
+        if(arrData[i]=='"' && arrData[i+1]=='t' && arrData[i+2]=='y'&& arrData[i+3]=='p' && arrData[i+4]=='e' && arrData[i+5]=='"')
+        {
+            a=String(number)+';';
+            i = i + 5;
+            number = number + 1;
         }
-        else if (arrData[i] == '"' && arrData[i+1] == ',' && arrData[i+2] == '"') {
-            line=',';
-            i=i+2;
+        else if(arrData[i]==':' && arrData[i+1]=='"' && arrData[i+2]=='t' && arrData[i+3]=='e' && arrData[i+4]=='x' && arrData[i+5] =='t' && arrData[i+6] =='"')
+        {
+            a='O;PL;';
+            i = i + 6;
         }
-        else if (arrData[i] == '"' && arrData[i+1] == ':' && arrData[i+2] == '[' && arrData[i+3] == '{' && arrData[i+4] == '"') {
-            line=':\n';
-            i=i+4;
-        } 
-        else if (arrData[i] == '"' && arrData[i+1] == '}' && arrData[i+2] == ',' && arrData[i+3] == '{' && arrData[i+4] == '"') {
-            line=',\n';
-            i=i+4;
+        else if(arrData[i]==':' && arrData[i+1]=='"' && arrData[i+2]=='r' && arrData[i+3]=='a' && arrData[i+4]=='d' && arrData[i+5]=='i' && arrData[i+6]=='o' && arrData[i+7]=='g' && arrData[i+8]=='r' && arrData[i+9]=='o' && arrData[i+10]=='u' && arrData[i+11]=='p' && arrData[i+12]=='"')
+        {
+            a='W;PL;';
+            i = i + 12;
         }
-        else if (arrData[i] == '{' && arrData[i+1] == '"' ) {
-            line='';
-            i=i+1;
+        else if(arrData[i]=='"' && arrData[i+1]=='t' && arrData[i+2]=='i'&& arrData[i+3]=='t' && arrData[i+4]=='l' && arrData[i+5]=='e' && arrData[i+6]=='"' && arrData[i+7]==':')
+        {
+            i = i + 9;
+            while (arrData[i]!='"')
+            {
+                a=a+arrData[i];
+                i = i + 1;
+            }
+            i = i + 2;
+            if (arrData[i]=='"' && arrData[i+1]=='c' && arrData[i+2]=='h' && arrData[i+3]=='o' && arrData[i+4]=='i' && arrData[i+5]=='c' && arrData[i+6]=='e' && arrData[i+7]=='s' && arrData[i+8]=='"')
+            {
+                i = i + 8;
+                a = a + ';';
+            }
+            else
+            {
+                a = a + ';|;\n'
+            }
         }
-        else if (arrData[i] == '"' || arrData[i] == '}' || arrData[i] == ']' ) {
-            line='';
+        else if(arrData[i]=='"' && arrData[i+1]=='t' && arrData[i+2]=='e' && arrData[i+3]=='x' && arrData[i+4]=='t' && arrData[i+5]=='"' && arrData[i+6]==':')
+        {
+            i = i + 8;
+            while (arrData[i]!='"')
+            {
+                a=a+arrData[i];
+                i = i + 1;
+            }
+            if (arrData[i]=='"' && arrData[i+1]=='}' && arrData[i+2]==']' && arrData[i+3]=='}')
+            {
+                a = a + ';\n';
+            }
+            else
+            {
+                a = a + ';';
+            }
         }
-        else line = arrData[i];
-
-        CSV += line;
+        else {
+            a = '';
+        }
+        endfile +=a;
 
     }
-
-    return CSV;
+    console.log(endfile);
+    return endfile;
 }
 
 
